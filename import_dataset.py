b7ace8c7cab3928fea78741bf313963de6f19858
pupu5003 <pnphuonguyen3005@gmail.com>
2024-12-04 14:15:08 +0700

    change dataset

--- import_dataset.py
+++ import_dataset.py
@@ -3,10 +3,12 @@ import shutil
 import os
 
 # Download dataset
-path = kagglehub.dataset_download("rounakbanik/the-movies-dataset")
+path = kagglehub.dataset_download("tmdb/tmdb-movie-metadata")
+
+print("Path to dataset files:", path)
 
 # Move files to the current directory
-current_folder = os.getcwd()
+current_folder = os.getcwd() + '/dataset'
 for file_name in os.listdir(path):
     shutil.move(os.path.join(path, file_name), current_folder)
 
