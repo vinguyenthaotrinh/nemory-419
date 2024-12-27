931e5c6f694f91d80a0935df370d77a2071becad
kknguyen214 <128380887+kknguyen214@users.noreply.github.com>
2024-12-27 16:15:04 +0700

    fix accept dup lib

--- image_search.py
+++ image_search.py
@@ -1,8 +1,11 @@
 import builtins
 from DeepImageSearch import Load_Data, Search_Setup
 
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
 # Override the input function to automatically respond 'no' without causing an error
-# builtins.input = lambda prompt="": "no"  # Automatically answers 'no' when input is called
+builtins.input = lambda prompt="": "no"  # Automatically answers 'no' when input is called
 
 def preprocess_image():
     image_list = Load_Data().from_folder(['poster'])
