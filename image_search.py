70f98a192dd1b43a16bb156f6896d4f509422f50
pupu5003 <pnphuonguyen3005@gmail.com>
2024-12-27 15:14:41 +0700

    update image search by resnet50

--- image_search.py
+++ image_search.py
@@ -2,7 +2,7 @@ import builtins
 from DeepImageSearch import Load_Data, Search_Setup
 
 # Override the input function to automatically respond 'no' without causing an error
-builtins.input = lambda prompt="": "no"  # Automatically answers 'no' when input is called
+# builtins.input = lambda prompt="": "no"  # Automatically answers 'no' when input is called
 
 def preprocess_image():
     image_list = Load_Data().from_folder(['poster'])
